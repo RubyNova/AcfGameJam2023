3bff827feee780ccb17d7e337da6b9b2dd2ffc4a
Matt Jones <matthewj866@gmail.com>
2023-03-19 02:55:19 +0000

    Basic walking, running and jumping.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,31 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [Header("Dependencies")]
-        [SerializeField] private Rigidbody2D _rigidbody;
+        [Header("Dependencies"), SerializeField] 
+        private Rigidbody2D _rigidbody;
 
-        [Header("Configuration")]
-        [SerializeField] private float _speed;
+        [Header("Configuration"), SerializeField]
+        private float _walkingSpeed;
 
-        private Vector2 _rawInput;
+        [SerializeField]
+        private float _runningSpeed;
 
-        private void FixedUpdate()
-        {
-            var moveAmountRaw = _rawInput * _speed;
-            var moveAmount = moveAmountRaw * Time.deltaTime;
-
-            _rigidbody.velocity = moveAmount;
-        }
+        [SerializeField]
+        private float _jumpForce;
 
         // Update is called once per frame
         private void Update()
         {
-            _rawInput.x = Input.GetAxisRaw("Horizontal");
+            Vector2 directionVector = new(Input.GetAxisRaw("Horizontal"), Input.GetKeyDown(KeyCode.Space) ? 1 : 0);
+            directionVector.x *= Input.GetKey(KeyCode.LeftShift) ? _runningSpeed : _walkingSpeed;
+            directionVector.y *= _jumpForce;
+            
+            if (Mathf.Approximately(directionVector.y, 0))
+            {
+                directionVector.y = _rigidbody.velocity.y;
+            }
+
+            _rigidbody.velocity = directionVector;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            _rigidbody.velocity = Vector2.zero;
         }
     }
 }
