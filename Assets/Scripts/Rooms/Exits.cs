82f91528ff8504bc1b28fb1ad519a47072fc2fde
GalaxyLittlepaws <galaxylittlepaws@proton.me>
2023-03-22 22:57:22 -0400

    Add in an Exit object for room traveling

---
+++ Assets/Scripts/Rooms/Exits.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exits : MonoBehaviour
+{
+    public GameObject roomToEnter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.GetComponent<Collider2D>().IsTouching(GameObject.Find("Player").GetComponent<Collider2D>()))
+        {
+            GameObject.Find("Player").transform.position = roomToEnter.transform.position;
+        }
+    }
+}
