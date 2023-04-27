2bc8d35aefd9ea2c13be0a55d44bd9aedd930837
Matt Jones <matthewj866@gmail.com>
2023-04-27 03:42:51 +0100

    Remove debug statement.

--- Assets/Scripts/Movement/GroundMover.cs
+++ Assets/Scripts/Movement/GroundMover.cs
@@ -98,9 +98,8 @@ namespace Movement
 
                 var directionToPosition = (entity.point - (Vector2)transform.position).normalized;
                 
-                if ((directionToPosition.x > 0 && directionVector.x > 0) || (directionToPosition.x < 0 && directionVector.x < 0))
+                if ((directionToPosition.x > 0 && directionVector.x > 0) || (directionToPosition.x < 0 && directionVector.x < 0))
                 {
-                    print($"conflict! Values: input dir: {directionVector.x} dirToPos: {directionToPosition.x}");
                     return true;
                 }
 
