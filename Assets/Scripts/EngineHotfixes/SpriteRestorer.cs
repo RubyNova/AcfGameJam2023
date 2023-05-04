a7932886f47d804af2c977ff19207db02677ca0a
Matt Jones <matthewj866@gmail.com>
2023-05-04 03:04:54 +0100

    Add stupid engine hotfix.

---
+++ Assets/Scripts/EngineHotfixes/SpriteRestorer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace EngineHotfixes
+{
+    public class SpriteRestorer : MonoBehaviour
+    {
+        [Serializable]
+        public class SpriteRendererDefaultConfig
+        {
+            [SerializeField]
+            private SpriteRenderer _spriteRenderer;
+
+            [SerializeField]
+            private Sprite _defaultSprite;
+
+            public void RestoreRendererToDefault()
+            {
+                _spriteRenderer.sprite = _defaultSprite;
+            }
+        }
+
+        [Header("Configuration"), SerializeField]
+        private SpriteRendererDefaultConfig[] _defaults;
+
+        public void RestoreSpriteDefaults()
+        {
+            foreach (var config in _defaults)
+            {
+                config.RestoreRendererToDefault();
+            }
+        }
+    }
+}
