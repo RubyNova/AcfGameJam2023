ff0eb19ff5074cb94d57347001bc5340c786efac
Matt Jones <matthewj866@gmail.com>
2023-05-04 22:14:26 +0100

    Add audio classifcation scriptable objects.

---
+++ Assets/Scripts/AudioManagement/ScriptableObjects/EntitySoundBank.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AudioManagement.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewEntitySoundBank", menuName = "Audio Management / Entity Sound Bank")]
+    public class EntitySoundBank : ScriptableObject
+    {
+        [SerializeField]
+        private List<AudioClip> _soundEffects;
+
+        public IReadOnlyList<AudioClip> SoundEffects => _soundEffects;
+    }
+}
