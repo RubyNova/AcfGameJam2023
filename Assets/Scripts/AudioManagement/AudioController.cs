53c3c8a11831d1d154e80db8952ee85da73d287e
Matt Jones <matthewj866@gmail.com>
2023-05-05 01:46:16 +0100

    Update audio scriptable.

--- Assets/Scripts/AudioManagement/AudioController.cs
+++ Assets/Scripts/AudioManagement/AudioController.cs
@@ -1,3 +1,5 @@
+using AudioManagement.ScriptableObjects;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -5,56 +7,120 @@ namespace AudioManagement
 {
     public class AudioController : MonoBehaviour
     {
-        [SerializeField] private AudioSource _musicTrack;
-        [FormerlySerializedAs("_volume"), SerializeField, Range(0, 1)] private float _musicVolume;
-        [SerializeField, Range(0, 1)] private float _soundEffectVolume;
-
-        private void Awake() => DontDestroyOnLoad(gameObject);
-
-        public float MusicVolume
+        public enum SoundtrackVariantState
         {
-            get => _musicVolume;
-            set
-            {
-                _musicVolume = _musicVolume switch
-                {
-                    < 0 => 0,
-                    > 1 => 1,
-                    _ => value
-                };
-
-                _musicTrack.volume = _musicVolume;
-            }
+            Calm,
+            Caution,
+            Danger
         }
+
+        [Header("Dependencies"), SerializeField]
+        public AudioSource _calmMusicSourceZero;
+
+        [SerializeField]
+        public AudioSource _calmMusicSourceOne;
+
+        [SerializeField]
+        public AudioSource _cautionMusicSourceZero;
+
+        [SerializeField]
+        public AudioSource _cautionMusicSourceOne;
         
-        public float SoundEffectVolume
+        [SerializeField]
+        public AudioSource _dangerMusicSourceZero;
+        
+        [SerializeField]
+        public AudioSource _dangerMusicSourceOne;
+
+        [Header("Configuration"), SerializeField]
+        private float _crossfadeSpeed; 
+        
+        private AreaSoundtrackVariantData _areaData;
+        private SoundtrackVariantState _areaState;
+
+        private int _currentCalmSourceIndex;
+        private int _currentCautionSourceIndex;
+        private int _currentDangerSourceIndex;
+
+        private AudioSource[] _calmSources;
+        private AudioSource[] _cautionSources;
+        private AudioSource[] _dangerSources;
+
+        private void Awake()
         {
-            get => _soundEffectVolume;
-            set
-            {
-                _soundEffectVolume = _soundEffectVolume switch
-                {
-                    < 0 => 0,
-                    > 1 => 1,
-                    _ => value
-                };
-            }
+            DontDestroyOnLoad(gameObject);
+            _areaState = SoundtrackVariantState.Calm;
+            _calmMusicSourceZero.volume = 0;
+            _cautionMusicSourceZero.volume = 0;
+            _dangerMusicSourceZero.volume = 0;
+            _calmMusicSourceOne.volume = 0;
+            _cautionMusicSourceOne.volume = 0;
+            _dangerMusicSourceOne.volume = 0;
+
+            _calmSources = { _calmMusicSourceZero, _calmMusicSourceOne };
+            _cautionSources = { _calmMusicSourceZero, _calmMusicSourceOne };
+            _dangerSources = { _calmMusicSourceZero, _calmMusicSourceOne };
+
         }
 
-        public void PlayMusic(AudioClip track, bool shouldLoop = true)
+        private IEnumerator CrossFade(SoundtrackVariantState newState, AreaSoundtrackVariantData newData = null)
         {
-            if (_musicTrack.isPlaying)
+            AudioClip clipToUseForTarget = null;
+            AudioSource audioSourceForTarget = null;
+            AudioSource audioSourceForCurrent = null;
+/*
+            switch (_areaState)
             {
-                _musicTrack.Stop();
+                case SoundtrackVariantState.Calm:
+                    audioSourceForCurrent = _calmMusicSource;
+                    break;
+                case SoundtrackVariantState.Caution:
+                    audioSourceForCurrent = _cautionMusicSource;
+                    break;
+                case SoundtrackVariantState.Danger:
+                    audioSourceForCurrent = _cautionMusicSource;
+                    break;
             }
-            
-            _musicTrack.loop = shouldLoop;
-            _musicTrack.clip = track;
-            _musicTrack.Play();
+
+            switch (newState)
+            {
+                case SoundtrackVariantState.Calm:
+                    clipToUseForTarget = newData != null ? newData.Calm : _areaData.Calm;
+                    audioSourceForTarget = _calmMusicSource;
+                    break;
+                case SoundtrackVariantState.Caution:
+                    clipToUseForTarget = newData != null ? newData.Caution : _areaData.Caution;
+                    audioSourceForTarget = _cautionMusicSource;
+                    break;
+                case SoundtrackVariantState.Danger:
+                    clipToUseForTarget = newData != null ? newData.Danger : _areaData.Danger;
+                    audioSourceForTarget = _dangerMusicSource;
+                    break;
+            }
+
+            if (_areaState == newState)
+            {
+                float curveTime = _crossfadeSpeed * 0.5; // slash it in half 
+            }
+            else
+            {
+
+            }
+*/
         }
 
-        public void PlayEffect(AudioClip effect) => AudioSource.PlayClipAtPoint(effect, Vector3.zero, SoundEffectVolume);
+        public void PlayAreaMusic(AreaSoundtrackVariantData areaData)
+        {
 
-        public void StopMusic() => _musicTrack.Stop();
+        }
+
+        public void PlayEffect(AudioClip effect)
+        {
+        }
+
+        public void StopMusic(bool fadeOut = true)
+        {
+
+        }
     }
 }
