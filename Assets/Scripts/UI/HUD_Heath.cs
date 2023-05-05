1066f37889f22bfbe47fdd360928b2dbf71fc73e
GalaxyLittlepaws <galaxylittlepaws@proton.me>
2023-05-05 00:55:08 -0400

    Working HUD state, fix health and inventory issues

--- Assets/Scripts/UI/HUD_Heath.cs
+++ Assets/Scripts/UI/HUD_Heath.cs
@@ -10,6 +10,14 @@ public class HUD_Health : MonoBehaviour
     [SerializeField] private Image _healthBarMask;
     [SerializeField] private Image _healthBarMask2;
     [SerializeField] private Image _healthBarMask3;
+    
+    [SerializeField] private PlayerHealthController _playerHealthController;
+    
+    public void Start()
+    {
+        _healthBarMask.fillAmount = (float)_playerHealthController.CurrentHealth / 100f;
+        _playerHealthController.OnHealthChanged += HandleHealthChanged;
+    }
 
     private void HandleHealthChanged(int currentHealth)
     {
