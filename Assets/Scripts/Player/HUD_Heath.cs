87cd6e579dd9e230cee8c9be4821ae7fe724799d
GalaxyLittlepaws <galaxylittlepaws@proton.me>
2023-05-04 22:04:46 -0400

    Fixing health bar logic to use new assets

--- Assets/Scripts/Player/HUD_Heath.cs
+++ Assets/Scripts/Player/HUD_Heath.cs
@@ -8,9 +8,13 @@ public class HUD_Health : MonoBehaviour
 {
 
     [SerializeField] private Image _healthBarMask;
+    [SerializeField] private Image _healthBarMask2;
+    [SerializeField] private Image _healthBarMask3;
 
     private void HandleHealthChanged(int currentHealth)
     {
         _healthBarMask.fillAmount = (float)currentHealth / 100f;
+        _healthBarMask2.fillAmount = (float)currentHealth / 100f;
+        _healthBarMask3.fillAmount = (float)currentHealth / 100f;
     }
 }
