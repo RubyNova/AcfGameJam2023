8ad54320c5a94756c10045c7ef934531eaf238c8
GalaxyLittlepaws <galaxylittlepaws@proton.me>
2023-05-08 01:14:00 -0400

    Working narration! Thanks Matt

---
+++ Assets/Scripts/DevHelpers/NarrativeSequenceUITester.cs
@@ -0,0 +1,17 @@
+﻿using ACHNarrativeDriver;
+using ACHNarrativeDriver.ScriptableObjects;
+using UnityEngine;
+
+namespace DevHelpers
+{
+    public class NarrativeSequenceUITester : MonoBehaviour
+    {
+        [SerializeField] private NarrativeUIController _target;
+        [SerializeField] private NarrativeSequence _targetSequence;
+
+        private void Start()
+        {
+            _target.ExecuteSequence(_targetSequence);
+        }
+    }
+}
