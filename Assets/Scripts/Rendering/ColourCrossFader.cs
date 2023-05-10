1997c4ad013bc628c98271d7be92698ce564d7fd
Matt Jones <matthewj866@gmail.com>
2023-05-10 04:18:24 +0100

    Make codebase compile.

--- Assets/Scripts/Rendering/ColourCrossFader.cs
+++ Assets/Scripts/Rendering/ColourCrossFader.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
-namespace Assets.Scripts.Rendering
+namespace Rendering
 {
-    internal class ColourCrossFader
+    public class ColourCrossFader : MonoBehaviour
     {
     }
 }
