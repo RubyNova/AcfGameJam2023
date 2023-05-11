dfb3de123c88643f8673974327068fed543def4a
Matt Jones <matthewj866@gmail.com>
2023-05-11 15:10:56 +0100

    commit before reset.

--- Assets/Scripts/SetPieceHelpers/NarrativeSequenceEndTrigger.cs
+++ Assets/Scripts/SetPieceHelpers/NarrativeSequenceEndTrigger.cs
@@ -11,16 +11,20 @@ namespace SetPieceHelpers
 
         private UnityAction onSequenceEndAction;
 
+        private bool _subscribed = false;
+
         public override void NotifyActiveStatus(bool isActiveRoom, Room roomContext, Vector2 playerEntryPosition = default)
         {
             onSequenceEndAction ??= new UnityAction(() => onSequenceEnd.Invoke());
 
             if (isActiveRoom)
             {
+                _subscribed = true;
                 MenuController.Instance.NarrativeMenu.sequenceFinishedEvent.AddListener(onSequenceEndAction);
             }
-            else
+            else if (_subscribed)
             {
+                _subscribed = false;
                 MenuController.Instance.NarrativeMenu.sequenceFinishedEvent.RemoveListener(onSequenceEndAction);
             }
         }
--- Assets/Scripts/Utilities/AsyncSceneSwitcher.cs
+++ Assets/Scripts/Utilities/AsyncSceneSwitcher.cs
@@ -20,9 +20,14 @@ namespace Utilities
 
         private Coroutine _sceneLoadingCoroutine;
 
+        public SceneToLoad? PreviousScene { get; private set; }
+        public SceneToLoad CurrentScene { get; private set; }
+
         protected override void OnInit()
         {
             _sceneLoadingCoroutine = null;
+            PreviousScene = null;
+            CurrentScene = (SceneToLoad)SceneManager.GetActiveScene().buildIndex;
         }
 
         public void SwitchScene(SceneToLoad newScene, Action<AsyncOperation> sceneReadyForActivationCallback)
@@ -38,6 +43,7 @@ namespace Utilities
             {
                 var asyncSceneLoad = SceneManager.LoadSceneAsync((int)newScene);
                 asyncSceneLoad.allowSceneActivation = false;
+                asyncSceneLoad.completed += _ => CurrentScene = newScene;
 
                 while (asyncSceneLoad.progress < 0.9f)
                 {
