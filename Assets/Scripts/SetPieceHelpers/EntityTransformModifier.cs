0d599aa40e573f81495777c7e08fc84e8238f159
Matt Jones <matthewj866@gmail.com>
2023-05-12 23:20:23 +0100

    Add entity transform modifier set piece helper.

---
+++ Assets/Scripts/SetPieceHelpers/EntityTransformModifier.cs
@@ -0,0 +1,35 @@
+﻿using Environment;
+using UnityEngine;
+
+namespace SetPieceHelpers
+{
+    public class EntityTransformModifier : RoomEntityBehaviour
+    {
+        [Header("Dependencies"), SerializeField]
+        private Transform _targetTransform;
+
+        [SerializeField]
+        private Vector2 _positionDiff;
+
+        [SerializeField]
+        private Vector3 _rotationDiff;
+
+        private bool _isActive = false;
+
+        public override void NotifyActiveStatus(bool isActiveRoom, Room roomContext, Vector2 playerEntryPosition = default)
+        {
+            _isActive = isActiveRoom;
+        }
+
+        public void ApplyModification()
+        {
+            if (!_isActive)
+            {
+                return;
+            }
+
+            _targetTransform.Translate(_positionDiff.x, _positionDiff.y, 0);
+            _targetTransform.Rotate(_rotationDiff);
+        }
+    }
+}
