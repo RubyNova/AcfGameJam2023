6b8aa0f118a98bf332edbc64c3e34509a1e801fb
Matt Jones <matthewj866@gmail.com>
2023-05-17 04:08:05 +0100

    Implement PlayerInputFilterer set piece component.

---
+++ Assets/Scripts/SetPieceHelpers/PlayerInputFilterer.cs
@@ -0,0 +1,36 @@
+﻿using Environment;
+using Player;
+using UnityEngine;
+
+namespace SetPieceHelpers
+{
+    public class PlayerInputFilterer : RoomEntityBehaviour
+    {
+        private bool _isActive = false;
+
+        public override void NotifyActiveStatus(bool isActiveRoom, Room roomContext, Vector2 playerEntryPosition = default)
+        {
+            _isActive = isActiveRoom;
+        }
+
+        public void Enable()
+        {
+            if (!_isActive)
+            {
+                return;
+            }
+
+            PlayerDeviceInputManager.Instance.EnableFiltering();
+        }
+
+        public void Disable()
+        {
+            if (!_isActive)
+            {
+                return;
+            }
+
+            PlayerDeviceInputManager.Instance.DisableFiltering();   
+        }
+    }
+}
