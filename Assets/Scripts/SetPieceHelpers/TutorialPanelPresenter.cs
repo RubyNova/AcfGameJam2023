bab3edc5edad759b9a74acb37c7bb1d18615593c
Matt Jones <matthewj866@gmail.com>
2023-05-22 03:26:07 +0100

    Add tutorial panel support to set pieces.

---
+++ Assets/Scripts/SetPieceHelpers/TutorialPanelPresenter.cs
@@ -0,0 +1,39 @@
+﻿using Environment;
+using UI;
+using UnityEngine;
+
+namespace SetPieceHelpers
+{
+    public class TutorialPanelPresenter : RoomEntityBehaviour
+    {
+        [SerializeField]
+        private int _tutorialPanelIndex;
+
+        private bool _active = false;
+
+        public override void NotifyActiveStatus(bool isActiveRoom, Room roomContext, Vector2 playerEntryPosition = default)
+        {
+            _active = isActiveRoom;
+        }
+
+        public void ShowPanel()
+        {
+            if (!_active)
+            {
+                return;
+            }
+            
+            MenuController.Instance.DialoguePlayer.EnableDialoguePage(_tutorialPanelIndex);
+        }
+
+        public void HidePanel()
+        {
+            if (!_active)
+            {
+                return;
+            }
+            
+            MenuController.Instance.DialoguePlayer.DisableDialoguePage(_tutorialPanelIndex);
+        }
+    }
+}
