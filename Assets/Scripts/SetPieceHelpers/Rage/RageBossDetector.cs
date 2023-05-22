bb470cb8cdbdbfbe11c3366db311c3af2099d2d3
Matt Jones <matthewj866@gmail.com>
2023-05-22 04:02:24 +0100

    Add Rage boss AI code for movement and jumping.

---
+++ Assets/Scripts/SetPieceHelpers/Rage/RageBossDetector.cs
@@ -0,0 +1,29 @@
+﻿using Environment;
+using SetPieceHelpers.BossFightHelpers;
+using UnityEngine;
+
+namespace SetPieceHelpers.Rage
+{
+    public class RageBossDetector : RoomEntityBehaviour
+    {
+        [Header("Dependencies"), SerializeField]
+        private JumpNode _nodeToControl;
+
+        private bool _active = false;
+
+        public override void NotifyActiveStatus(bool isActiveRoom, Room roomContext, Vector2 playerEntryPosition = default)
+        {
+            _active = isActiveRoom;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!_active || !collision.gameObject.TryGetComponent<RageNPCCore>(out var core))
+            {
+                return;
+            }
+
+            core.NotifyJumpNodeReached(_nodeToControl);
+        }
+    }
+}
